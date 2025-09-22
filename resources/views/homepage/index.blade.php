c96807406179f71ffd9a10d4edbd21bb00f9df23
andiyanuar <andiachmadyanuar13@gmail.com>
2025-09-22 14:46:11 +0700

    Menambahkan folder views & menghubungkan route

---
+++ resources/views/homepage/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage perusahaan</title>
+</head>
+<body>
+    <h1>Cipta Digital | One Stop Business Solution</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
+/* Homepage & Landingpage */
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage.index');
+})->name('homepage.index');
+
+/* Auth Routes */
+Route::prefix('auth')->group(function(){
+
+    ## Routing Login
+    Route::get('/login', function(){
+        return view('auth.login');
+    })->name('auth.login');
+
+    Route::get('/register', function(){
+        return view('auth.register');
+    })->name('auth.register');
 });
+
+/* Homepage Feature */
+Route::prefix('homepage')->group(function(){
+
+    ## Product
+    Route::get('/products', function(){
+        return view('homepage.products');
+    })->name('homepage.products');
+
+    ## Cart
+    Route::get('/cart', function(){
+        return view('homepage.cart');
+    })->name('homepage.cart');
+});
+
