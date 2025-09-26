6d86f78211d60c73ebea5fc3c35bfcc2d4a73654
andiyanuar <andiachmadyanuar13@gmail.com>
2025-09-26 11:59:35 +0700

    fix logika pada seeders harga * jumlahnya agar sesuai

--- database/seeders/OrderDetailsSeeder.php
+++ database/seeders/OrderDetailsSeeder.php
@@ -12,17 +12,23 @@ class OrderDetailsSeeder extends Seeder
     {
         // Ambil order terakhir
         $order = Order::latest()->first();
+
         // Ambil produk secara acak
         $product = Product::inRandomOrder()->first();
+
+        // Tentukan jumlah produk
+        $jumlah = rand(1, 3);
 
         // Tambahkan order detail
         $orderDetail = $order->orderDetails()->create([
             'product_id' => $product->product_id,
-            'jumlah' => rand(1, 3),
-            'harga' => $product->harga,
+            'jumlah' => $jumlah,
+            'harga' => $product->harga * $jumlah, // total harga untuk detail ini
         ]);
 
-        // Update total harga order
-        $order->update(['total_harga' => $orderDetail->jumlah * $orderDetail->harga]);
+        // Update total harga order berdasarkan semua order details
+        $order->update([
+            'total_harga' => $order->orderDetails->sum(fn($detail) => $detail->harga)
+        ]);
     }
 }
